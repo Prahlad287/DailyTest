ca86a4393b13d438e6cfb4bb3193d9258e022ca5
DELL <DELL@DESKTOP-TK9LMLV>
2023-12-11 13:48:26 +0530

    new

--- TestPractice/src/com/test07dec/Student.java
+++ TestPractice/src/com/test07dec/Student.java
@@ -1,15 +1,21 @@
 package com.test07dec;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
 import java.util.TreeSet;
 
-class Student5 {
+class Student5  {
 	int id;
 	String nameString;
-	public Student5(int id, String nameString) {
+	String address;
+	public Student5(int id, String nameString, String address) {
 		super();
 		this.id = id;
 		this.nameString = nameString;
+		this.address = address;
 	}
 
 	public Student5() {
@@ -17,7 +23,7 @@ class Student5 {
 	}
 	@Override
 	public String toString() {
-		return "Student [id=" + id + ", nameString=" + nameString + "]";
+		return "Student [id=" + id + ",    nameString=" + nameString +",    Address=" + address + "]";
 	}
 }
 
@@ -32,48 +38,91 @@ class SortingById implements Comparator<Student5> {
 
 	public int compare(Student5 o1, Student5 o2) {
 		if (o1.id > o2.id) {
-			return -1;
-		} else if (o1.id < o2.id) {
 			return 1;
+		} else if (o1.id < o2.id) {
+			return -1;
 		}
 		return 0;
+	}
+}
+class SortAddress implements Comparator<Student5> {
+	@Override
+	public int compare(Student5 o1, Student5 o2) {
+		return o1.address.compareTo(o2.address);
 	}
 }
 
 class Student {
 
 	public static void main(String[] args) {
-		Student5 student = new Student5(10, "Ram");
-		Student5 student2 = new Student5(20, "Ramesh");
-		Student5 student3 = new Student5(10, "Preveen");
-		Student5 student4 = new Student5(30, "Santosh");
-		Student5 student5 = new Student5(60, "Jitendar");
-		Student5 student6 = new Student5(80, "Jitendarit");
-		Student5 student7 = new Student5(100, "Rahul");
-		Student5 student8 = new Student5(101, "Rahul");
+		ArrayList<Student5> studentList = new ArrayList<>();
 
-		TreeSet<Student5> set = new TreeSet<Student5>(new SortingById());
-		set.add(student);
-		set.add(student2);
-		set.add(student3);
-		set.add(student4);
-		set.add(student5);
-		set.add(student6);
-		set.add(student7);
-		set.add(student8);
-		set.forEach(System.out::println);
-		System.out.println("************************************************");
+	        // Creating Student5 objects
+	        Student5 student1 = new Student5(10, "Ram","Jaipur");
+	        Student5 student2 = new Student5(20, "Ramesh","Alwar");
+	        Student5 student3 = new Student5(10, "Preveen","Jodhpur");
+	        Student5 student4 = new Student5(30, "Santosh", "Sirohi");
+	        Student5 student5 = new Student5(60, "Jitendar","Jaipur");
+	        Student5 student6 = new Student5(80, "Jitendar Kumar", "Sikar");
+	        Student5 student7 = new Student5(100, "Rahul","PratapGarh");
+	        Student5 student8 = new Student5(101, "Rahul","Sikar");
 
-		TreeSet<Student5> set1 = new TreeSet<Student5>(new StringSort());
-		set1.add(student);
-		set1.add(student2);
-		set1.add(student3);
-		set1.add(student4);
-		set1.add(student5);
-		set1.add(student6);
-		set1.add(student7);
-		set1.add(student8);
-		
-		set1.forEach(System.out::println);
+	        // Adding objects to the list
+	        studentList.add(student1);
+	        studentList.add(student2);
+	        studentList.add(student3);
+	        studentList.add(student4);
+	        studentList.add(student5);
+	        studentList.add(student6);
+	        studentList.add(student7);
+	        studentList.add(student8);
+	        TreeSet<Student5> set = new TreeSet<Student5>(new SortingById());
+	        set.addAll(studentList);
+	        TreeSet<Student5> set1 = new TreeSet<Student5>(new StringSort());
+	        set1.addAll(studentList);	
+	        TreeSet<Student5> set2 = new TreeSet<Student5>(new SortAddress());
+	        set2.addAll(studentList);		
+	        while (true) {
+	        System.out.println("Enter number 1 for Sorting By Id");
+	        System.out.println("Enter number 2 for Sorting By Name");
+	        System.out.println("Enter number 3 for Sorting By Address");
+	        System.out.println("Enter number 4 for Reverse Sorting By Address");
+	        System.out.println("Enter number 5 for Reverse Sorting Name");
+	        System.out.println("Enter number 6 for Reverse Sorting Id");
+	        Scanner s = new Scanner(System.in); 
+				int key = s.nextInt();
+				switch (key) {
+				case 1: {
+					set.forEach(System.out::println);
+					break;
+				}case 2:{					
+					set1.forEach(System.out::println);
+					break;
+				}case 3:{										
+					set2.forEach(System.out::println);
+					break;
+				}case 4:{	
+					set2=(TreeSet<Student5>) set2.descendingSet();
+					set2.forEach(System.out::println);
+					break;
+				}case 5:{	
+					set1=(TreeSet<Student5>) set1.descendingSet();
+					set1.forEach(System.out::println);
+					break;
+				}case 6: {
+					set=(TreeSet<Student5>) set.descendingSet();
+					set.forEach(System.out::println);
+					break;
+				}
+				default:
+					throw new IllegalArgumentException("Unexpected value: " + key);
+				
+				}
+				System.out.println("Again? (1 for Yes, 0 for No)");
+	            int again = s.nextInt();
+	            if (again == 0) {
+	                break;
+	            }
+	        }  
 	}
 }
